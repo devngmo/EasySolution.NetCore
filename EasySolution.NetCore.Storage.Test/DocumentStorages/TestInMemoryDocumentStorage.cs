9f26c066757d67cd4d0943edef5e8122ba1f9b73
Tran Manh Linh <devngmo@gmail.com>
2022-10-04 14:54:27 +0700

    add test serialize massive documents

--- EasySolution.NetCore.Storage.Test/DocumentStorages/TestInMemoryDocumentStorage.cs
+++ EasySolution.NetCore.Storage.Test/DocumentStorages/TestInMemoryDocumentStorage.cs
@@ -21,10 +21,43 @@ namespace EasySolution.NetCore.Storage.Test.DocumentStorages
             AddDocumentResult result = storage.Add(book);
             Assert.IsNotNull(result.DocumentId);
 
-            var bookOnStorage = storage.Get(result.DocumentId);
+            DocumentRecord<dynamic> bookOnStorage = storage.Get(result.DocumentId);
             Assert.IsNotNull(bookOnStorage);
-            Assert.IsNotNull("Awesome Book", "" + bookOnStorage.title);
-            Assert.IsNotNull("Mr.Awesome", "" + bookOnStorage.author);
+            Assert.IsNotNull(result.DocumentId, "" + bookOnStorage._id);
+            Assert.IsNotNull("Awesome Book", "" + bookOnStorage.source.title);
+            Assert.IsNotNull("Mr.Awesome", "" + bookOnStorage.source.author);
+        }
+
+        [TestMethod]
+        public void serializeMassiveDocuments()
+        {
+            List<AddDocumentResult> resultList = new List<AddDocumentResult>();
+            int nDocs = 10000000;
+
+            Console.WriteLine($"Add {nDocs} documents...");
+            DateTime start = DateTime.Now;
+            for (int i = 0; i < nDocs; i++)
+            {
+                var book = new { title = $"Awsome Book {i}", author = $"Mr.Awesome {i}" };
+                AddDocumentResult result = storage.Add(book);
+                Assert.IsNotNull(result.DocumentId);
+                resultList.Add(result);
+            }
+            TimeSpan duration = DateTime.Now - start;
+            Console.WriteLine($"Add {nDocs} documents take {duration.TotalSeconds} seconds");
+
+            Console.WriteLine($"Load and Validate {nDocs} documents...");
+            start = DateTime.Now;
+            for (int i = 0; i < nDocs; i++)
+            {
+                DocumentRecord<dynamic> bookOnStorage = storage.Get(resultList[i].DocumentId);
+                Assert.IsNotNull(bookOnStorage);
+                Assert.IsNotNull(resultList[i].DocumentId, "" + bookOnStorage._id);
+                Assert.IsNotNull($"Awesome Book {i}", "" + bookOnStorage.source.title);
+                Assert.IsNotNull($"Mr.Awesome {i}", "" + bookOnStorage.source.author);
+            }
+            duration = DateTime.Now - start;
+            Console.WriteLine($"Load and validate {nDocs} documents take {duration.TotalSeconds} seconds");
         }
     }
 }
